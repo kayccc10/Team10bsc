be59ad976bb0e48edb1dd14bfaef9e5b95f2da6c
ErvinDimitri <44121451+ErvinDimitri@users.noreply.github.com>
2021-04-02 02:54:35 +0200

    Update IdVerify.sol

--- contracts/IdVerify.sol
+++ contracts/IdVerify.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
 contract IdVerify {
@@ -40,7 +39,10 @@ contract IdVerify {
   mapping(address => UserInfo) public Users;
   mapping(uint => mapping(address => UserId)) public userIds;
   //mapping(address => IssuedId) public IssuedIds;
-
+    mapping (address => address[]) requestInstitutions;  // Address of all institutions that sent request  to a specific user
+    mapping (address => mapping(address => bool[]))  dataRequest;  // Data requested by the institution
+    mapping (address => mapping(address => bool[])) approvedRequest;
+    mapping (address => string) public institutionInfo;
 
   address public owner;
 
@@ -63,4 +65,25 @@ contract IdVerify {
     userIds[fileCount][owner] = UserId(_idNo, _idName, _idDob, _idHash, _idHomeAddress, msg.sender);
     emit idCreated(_idNo, _idName, _idDob, _idHash, _idHomeAddress, msg.sender);
   }
+
+  function registerInstitution(string _name)public {
+        institutionInfo[msg.sender] = _name;
+    }
+    function sendRequest(address _a, bool[] memory _data) public{  // Institution Side
+        requestInstitutions[_a].push(msg.sender);
+        dataRequest[_a][msg.sender] = _data;
+    }
+    function getInstitutionAddr() public view returns(address [] memory){  // User Side   List of institutions that sent request
+        return requestInstitutions[msg.sender];
+    }
+    function getRequest(address _a) public view returns(bool [] memory){  //User Side
+        return dataRequest[msg.sender][_a];
+    }
+    function approveRequest(uint _id, bool [] memory _approvedData ) public{
+        address _a = requestInstitutions[msg.sender][_id];
+        approvedRequest[msg.sender][_a] = _approvedData;
+    }
+    function getApprovedRequest(address _a) public view  returns(bool [] memory){
+        return approvedRequest[msg.sender][_a];
+    }
 }
