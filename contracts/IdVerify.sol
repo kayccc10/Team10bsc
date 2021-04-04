1d21f1b0c5837667dc4a7b3450e4a14226f612e1
abdoulore <78310709+abdoulore@users.noreply.github.com>
2021-04-04 17:56:22 +0100

    Update IdVerify.sol

--- contracts/IdVerify.sol
+++ contracts/IdVerify.sol
@@ -17,7 +17,11 @@ contract IdVerify {
     uint phoneNo,
     address uploader
   );
-
+  
+  // fullName The full name of the applicant
+  // emailAddress The email address of the applicant
+  // phoneNo The applicant mobile number
+  
   struct UserId {
     uint idNo;
     string idName;
@@ -26,6 +30,11 @@ contract IdVerify {
     string idHomeAddress;
     address uploader;
   }
+  // idNo The number on the applicant identity card
+  // idName The type of identity card used
+  // idDob The date of birth of the applicant as shown on the identity card
+  // idHash The hash generated when Tendity account is created
+  // idHomeAddress The current residential address of the applicant
 
   event idCreated    (
     uint idNo,
@@ -38,7 +47,7 @@ contract IdVerify {
 
   mapping(address => UserInfo) public Users;
   mapping(uint => mapping(address => UserId)) public userIds;
-  //mapping(address => IssuedId) public IssuedIds;
+    mapping(address => IssuedId) public IssuedIds;
     mapping (address => address[]) requestInstitutions;  // Address of all institutions that sent request  to a specific user
     mapping (address => mapping(address => bool[]))  dataRequest;  // Data requested by the institution
     mapping (address => mapping(address => bool[])) approvedRequest;
@@ -68,21 +77,39 @@ contract IdVerify {
   function registerInstitution(string memory _name)public {
         institutionInfo[msg.sender] = _name;
     }
-    function sendRequest(address _a, bool[] memory _data) public{  // Institution Side
+    
+    // Institutions does their registration here
+    
+    function sendRequest(address _a, bool[] memory _data) public{  
         requestInstitutions[_a].push(msg.sender);
         dataRequest[_a][msg.sender] = _data;
     }
-    function getInstitutionAddr() public view returns(address [] memory){  // User Side   List of institutions that sent request
+    
+    // Institutions makes request for a specific user information here
+    
+    function getInstitutionAddr() public view returns(address [] memory){  
         return requestInstitutions[msg.sender];
     }
-    function getRequest(address _a) public view returns(bool [] memory){  //User Side
+    
+    // User Side   List of institutions that sent request
+    
+    function getRequest(address _a) public view returns(bool [] memory){  
         return dataRequest[msg.sender][_a];
     }
-    function approveRequest(uint _id, bool [] memory _approvedData ) public{
+    
+    // User receives the Institution request for information
+    
+    function approveRequest(uint _id, bool [] memory _approvedData ) public{  
         address _a = requestInstitutions[msg.sender][_id];
         approvedRequest[msg.sender][_a] = _approvedData;
     }
+    
+    // User determines what information to be released to the institution
+    // User is allowed to reject the institution request
+    
     function getApprovedRequest(address _a) public view  returns(bool [] memory){
         return approvedRequest[msg.sender][_a];
     }
+    
+    // The institution receives the the released information
 }
