d29154651028fead7b33adf8ef493331a7e47110
“Makena” <makenaroes@gmail.com>
2021-04-08 13:52:22 +0300

    minor variable rename

--- tendity/src/App.js
+++ tendity/src/App.js
@@ -8,8 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      manager: '',
-      players: [],
+      owner: '',
       balance: '',
       value: '',
       message: ''
@@ -22,18 +21,18 @@ class App extends Component {
   }
 
   render() {
-    const {manager, players, balance} = this.state
+    const {owner, value, balance} = this.state
     return (
         <div className="container">
           <Navbar/>
           <header className="body">
-            <h3>Welcome to identity verifivation</h3>
+            <h3>Welcome to Tendity</h3>
           </header>
           <body>
           <h5>Info</h5>
           <p>There are currently no verifiers</p>
           <hr/>
-          <FormControl onSubmit='' className="row g-3">
+          <form onSubmit='' className="row g-3">
             <div className="col-auto">
               <label htmlFor="idNo" className="visually-hidden">Amount to enter</label>
                 <input
@@ -44,39 +43,39 @@ class App extends Component {
                 <input
                     value={this.state.value}
                     onChange={e => this.setState({value: e.target.value})}
-                    type="amount" className="form-control" id="amount"
+                    type="email" className="form-control" id="email"
                     placeholder="email"/>
                 <input
                     value={this.state.value}
                     onChange={e => this.setState({value: e.target.value})}
-                    type="amount" className="form-control" id="amount"
+                    type="number" className="form-control" id="phoneNo"
                     placeholder="phone no"/>
               <input
                   value={this.state.value}
                   onChange={e => this.setState({value: e.target.value})}
-                  type="amount" className="form-control" id="amount"
+                  type="text" className="form-control" id="idNo"
                   placeholder="id no"/>
                 <input
                     value={this.state.value}
                     onChange={e => this.setState({value: e.target.value})}
-                    type="amount" className="form-control" id="amount"
+                    type="date" className="form-control" id="dob"
                     placeholder="date of birth"/>
                 <input
                     value={this.state.value}
                     onChange={e => this.setState({value: e.target.value})}
-                    type="amount" className="form-control" id="amount"
+                    type="text" className="form-control" id="homeAddress"
                     placeholder="home address"/>
-                <input
-                    value={this.state.value}
-                    onChange={e => this.setState({value: e.target.value})}
-                    type="amount" className="form-control" id="amount"
-                    placeholder="verfication amount"/>
-                    <p>To interact with the contract, you need some ether.</p>
+                {/*<input*/}
+                {/*    value={this.state.value}*/}
+                {/*    onChange={e => this.setState({value: e.target.value})}*/}
+                {/*    type="amount" className="form-control" id="verficationAmount"*/}
+                {/*    placeholder="verfication amount"/>*/}
+                    <p>Note! To interact with the contract, you need some BNB(0.001).</p>
               <div className="col-auto">
                 <button type="submit" className="btn btn-primary mb-3">Submit</button>
               </div>
             </div>
-          </FormControl>
+          </form>
           <hr/>
 
           <button onClick='' type="button" className="btn btn-success">Verify</button>
