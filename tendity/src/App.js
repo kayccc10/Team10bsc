6cead5fa9c25980e9e57cf51858a812ae5051d6b
“Makena” <makenaroes@gmail.com>
2021-04-09 16:24:51 +0300

    add labels

--- tendity/src/App.js
+++ tendity/src/App.js
@@ -5,86 +5,127 @@ import {Component} from "react/cjs/react.production.min";
 import Navbar from "./components/navbar";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      owner: '',
-      balance: '',
-      value: '',
-      message: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            owner: '',
+            balance: '',
+            value: '',
+            message: '',
+            idNo: '',
+            idName: '',
+            idDob: '',
+            idHash: '',
+            idHomeAddress: ''
+        }
     }
-  }
 
-  async componentDidMount() {
-    const manager = await idVerify.methods.owner().call();
-    this.setState({manager});
-  }
+    onCreateUser = async () => {
+        const accounts = await web3.eth.getAccounts()
+        this.setState({message: 'Waiting for confirmation...'})
 
-  render() {
-    const {owner, value, balance} = this.state
-    return (
-        <div className="container">
-          <Navbar/>
-          <header className="body">
-            <h3>Welcome to Tendity</h3>
-          </header>
-          <body>
-          <h5>Info</h5>
-          <p>There are currently no verifiers</p>
-          <hr/>
-          <form onSubmit='' className="row g-3">
-            <div className="col-auto">
-              <label htmlFor="idNo" className="visually-hidden">Amount to enter</label>
-                <input
-                    value={this.state.value}
-                    onChange={e => this.setState({value: e.target.value})}
-                    type="amount" className="form-control" id="amount"
-                    placeholder="full name"/>
-                <input
-                    value={this.state.value}
-                    onChange={e => this.setState({value: e.target.value})}
-                    type="email" className="form-control" id="email"
-                    placeholder="email"/>
-                <input
-                    value={this.state.value}
-                    onChange={e => this.setState({value: e.target.value})}
-                    type="number" className="form-control" id="phoneNo"
-                    placeholder="phone no"/>
-              <input
-                  value={this.state.value}
-                  onChange={e => this.setState({value: e.target.value})}
-                  type="text" className="form-control" id="idNo"
-                  placeholder="id no"/>
-                <input
-                    value={this.state.value}
-                    onChange={e => this.setState({value: e.target.value})}
-                    type="date" className="form-control" id="dob"
-                    placeholder="date of birth"/>
-                <input
-                    value={this.state.value}
-                    onChange={e => this.setState({value: e.target.value})}
-                    type="text" className="form-control" id="homeAddress"
-                    placeholder="home address"/>
-                {/*<input*/}
-                {/*    value={this.state.value}*/}
-                {/*    onChange={e => this.setState({value: e.target.value})}*/}
-                {/*    type="amount" className="form-control" id="verficationAmount"*/}
-                {/*    placeholder="verfication amount"/>*/}
-                    <p>Note! To interact with the contract, you need some BNB(0.001).</p>
-              <div className="col-auto">
-                <button type="submit" className="btn btn-primary mb-3">Submit</button>
-              </div>
+        await idVerify.methods.idRequest().send({
+            from: accounts[0]
+        });
+        this.setState({message: 'Winner has been picked!'})
+    }
+
+    async componentDidMount() {
+        const manager = await idVerify.methods.owner().call();
+        this.setState({manager});
+    }
+
+    render() {
+        const {owner, value, balance} = this.state
+        return (
+            <div className="container" style={{opacity: "80%"}}>
+                <Navbar/>
+
+                <header className="card">
+                    <h3 style={{color: "gray"}}>Welcome to Tendity</h3>
+                </header>
+                <body >
+                <div className="container">
+                <h5>Info</h5>
+                <p>There are currently no verifiers</p>
+                </div>
+                <hr/>
+                <form onSubmit='' className="row g-3">
+                    <div className="col-auto" style={{padding: "30px"}}>
+                        <div className="mb-3">
+                            <label htmlFor="exampleFormControlInput1" className="form-label">Full name</label>
+                            <input
+                                value={this.state.value}
+                                onChange={e => this.setState({value: e.target.value})}
+                                type="amount" className="form-control" id="amount"
+                                placeholder="full name"/>
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="exampleFormControlInput1" className="form-label">Email</label>
+
+                        <input
+                            value={this.state.value}
+                            onChange={e => this.setState({value: e.target.value})}
+                            type="email" className="form-control" id="email"
+                            placeholder="email"/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="exampleFormControlInput1" className="form-label">Phone Number</label>
+
+                        <input
+                            value={this.state.value}
+                            onChange={e => this.setState({value: e.target.value})}
+                            type="number" className="form-control" id="phoneNo"
+                            placeholder="phone no"/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="exampleFormControlInput1" className="form-label">ID Number</label>
+
+                        <input
+                            value={this.state.value}
+                            onChange={e => this.setState({value: e.target.value})}
+                            type="text" className="form-control" id="idNo"
+                            placeholder="id no"/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="exampleFormControlInput1" className="form-label">Date of Birth</label>
+
+                        <input
+                            value={this.state.value}
+                            onChange={e => this.setState({value: e.target.value})}
+                            type="date" className="form-control" id="dob"
+                            placeholder="date of birth"/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="exampleFormControlInput1" className="form-label">Home Address</label>
+
+                        <input
+                            value={this.state.value}
+                            onChange={e => this.setState({value: e.target.value})}
+                            type="text" className="form-control" id="homeAddress"
+                            placeholder="home address"/>
+                        </div>
+                        {/*<input*/}
+                        {/*    value={this.state.value}*/}
+                        {/*    onChange={e => this.setState({value: e.target.value})}*/}
+                        {/*    type="amount" className="form-control" id="verficationAmount"*/}
+                        {/*    placeholder="verfication amount"/>*/}
+                        <p>Note! To interact with the contract, you need some BNB(0.001).</p>
+                        <div className="col-auto">
+                            <button type="submit" className="btn btn-primary mb-3">Submit</button>
+                        </div>
+                    </div>
+                </form>
+                <hr/>
+
+                <button onClick='' type="button" className="btn btn-success">Verify</button>
+                <hr/>
+                <h4>{this.state.message}</h4>
+                </body>
             </div>
-          </form>
-          <hr/>
-
-          <button onClick='' type="button" className="btn btn-success">Verify</button>
-          <hr/>
-          <h4>{this.state.message}</h4>
-          </body>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
